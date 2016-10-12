43b0774a4b0333b690ca7bb2746d6a1abd61ab7c
Kristie A Bedgood <kristieabedgood@Kristies-MacBook-Pro.local>
2016-10-11 21:58:08 -0400

    fixing stuff i broke

---
+++ main.js
@@ -0,0 +1,39 @@
+function renderTime() {
+
+	var currentTime = new Date ();
+	var amPM = "AM";
+	var hour = currentTime.getHours ();
+	var minutes = currentTime.getMinutes();
+	var seconds = currentTime.getSeconds();
+
+if (hour === 0) {
+	hour = 12;
+}
+else (hour > 12) 
+{
+	hour = hour - 12;
+	amPM = "PM";
+}
+
+if (hour < 10) {
+	hour = "0" + hour;
+}
+
+if (minutes < 10) {
+	minutes = "0" + minutes;
+}
+
+if (seconds < 10) {
+	seconds= "0" + seconds;
+}
+
+var myClock = document.getElementById("currentTime");
+myClock.innerHTML = hour + ":" + minutes + ":" + seconds + amPM;
+}
+
+setInterval (renderTime,1000);
+
+// moment(shirt.date).format("MMMM Do YYYY, h:mm a");
+
+
+
